ff6e3dbb434dad9ba97dda7086f6a3bdcdbb795a
laborerac367 <laborerac367@strose.edu>
2018-03-30 13:05:02 -0400

    Update Backend-SQLscript.sql

--- Backend-SQLscript.sql
+++ Backend-SQLscript.sql
@@ -1,7 +1,6 @@
-DROP DATABASE IF EXISTS dpsbackend;
-CREATE DATABASE dpsbackend;
-
-SELECT dpsbackend;
+DROP DATABASE DPSbackend;
+CREATE DATABASE DPSbackend;
+USE DPSbackend;
 
 DROP TABLE IF EXISTS UserName;
 CREATE TABLE UserName(
@@ -10,6 +9,13 @@ CREATE TABLE UserName(
     FirstName VARCHAR(255),
     PRIMARY KEY (PID) 
 );
+INSERT INTO UserName(PID, LastName, FirstName) VALUES (001, 'Ganoush', 'Baba' ); 
+INSERT INTO UserName(PID, LastName, FirstName) VALUES (002, 'Lovelace', 'Betty' ); 
+INSERT INTO UserName(PID, LastName, FirstName) VALUES (003, 'Jordan', 'Hal' ); 
+INSERT INTO UserName(PID, LastName, FirstName) VALUES (004, 'Wiggen', 'Ender' ); 
+INSERT INTO UserName(PID, LastName, FirstName) VALUES (005, 'Arkanian', 'Petra' ); 
+INSERT INTO UserName(PID, LastName, FirstName) VALUES (006, 'Wiggin', 'Peter' );
+INSERT INTO UserName(PID, LastName, FirstName) VALUES (999, 'User', 'General' ); 
 
 
 DROP TABLE IF EXISTS UserPassword;
@@ -39,15 +45,15 @@ INSERT INTO UserInfo(PID, address, phoneNumber, userEmail, RID) VALUES (999, 'NU
 
 
 DROP TABLE IF EXISTS Event;
-CREATE TABLE Event 
-(EID INT, 
-FirstName VARCHAR(50), 
-LastName VARCHAR(50), 
-Date DATE, 
-Address VARCHAR(30),
-City VARCHAR(20),
-State VARCHAR(20),
-PRIMARY KEY(EID,Date)
+CREATE TABLE Event (
+	EID INT, 
+	FirstName VARCHAR(50), 
+	LastName VARCHAR(50), 
+	Date DATE, 
+	Address VARCHAR(30),
+	City VARCHAR(20),
+	State VARCHAR(20),
+	PRIMARY KEY(EID,Date)
 );
 
 INSERT INTO Event(EID, FirstName, LastName, Date, Address, City, State) VALUES(001, 'Erick', 'Salas', '2014-04-01', '2 This ave', 'Nowear', 'Alter');
@@ -55,15 +61,14 @@ INSERT INTO Event(EID, FirstName, LastName, Date, Address, City, State) VALUES(0
 INSERT INTO Event(EID, FirstName, LastName, Date, Address, City, State) VALUES(003, 'Thing', 'Twou', '1997-08-02', 'Mono', 'Harlem', 'New York');
 INSERT INTO Event(EID, FirstName, LastName, Date, Address, City, State) VALUES(004, 'Christ', 'Jesus', '0001-01-01', 'Jerus av', 'Truths', 'News');
 INSERT INTO Event(EID, FirstName, LastName, Date, Address, City, State) VALUES(005, 'Gero', 'Cornell', '1980-09-12', '2 This ave', 'Nowear', 'Alter');
-INSERT INTO Event(EID, FirstName, LastName, Date, Address, City, State) VALUES(006, 'Smort', 'Ace', '2010-06-051', '2 This ave', 'Nowear', 'Alter');
-INSERT INTO Event(EID, FirstName, LastName, Date, Address, City, State) VALUES(999, 'NULL', 'NULL', '0000-00-00', 'NULL', 'NULL', 'NULL');
-
+INSERT INTO Event(EID, FirstName, LastName, Date, Address, City, State) VALUES(006, 'Smort', 'Ace', '2010-06-05', '2 This ave', 'Nowear', 'Alter');
+INSERT INTO Event(EID, FirstName, LastName, Date, Address, City, State) VALUES(999, 'NULL', 'NULL', '1899-01-01', 'NULL', 'NULL', 'NULL');
 
 DROP TABLE IF EXISTS EventRoster;
-CREATE TABLE EventRoster
-(EID INT,
-PID INT,
-PRIMARY KEY(PID,EID)
+CREATE TABLE EventRoster(
+	EID INT,
+	PID INT,
+	PRIMARY KEY(PID,EID)
 ); 
 
 INSERT INTO EventRoster(EID, PID) VALUES(001, 001);
@@ -76,68 +81,56 @@ INSERT INTO EventRoster(EID, PID) VALUES(999, 999);
 
 
 DROP TABLE IF EXISTS Contributions;
-CREATE TABLE Contributions
-(EID INT, 
-PID INT, 
-Amount DECIMAL,
-PRIMARY KEY (PID,EID)
+CREATE TABLE Contributions(
+	EID INT, 
+	PID INT, 
+	Amount   DECIMAL(13, 2),
+	PRIMARY KEY (PID,EID)
 ); 
 
-INSERT INTO Contribution(EID, PID, Amount) VALUES(001, 001, 1000.00);
-INSERT INTO Contribution(EID, PID, Amount) VALUES(002, 002, 421.20);
-INSERT INTO Contribution(EID, PID, Amount) VALUES(003, 003, 2222.22);
-INSERT INTO Contribution(EID, PID, Amount) VALUES(004, 004, 1.01);
-INSERT INTO Contribution(EID, PID, Amount) VALUES(005, 005, 0.01);
-INSERT INTO Contribution(EID, PID, Amount) VALUES(006, 006, 100000.10);
-INSERT INTO Contribution(EID, PID, Amount) VALUES(999, 999, NULL);
+INSERT INTO Contributions(EID, PID, Amount) VALUES(001, 001, 1000.00);
+INSERT INTO Contributions(EID, PID, Amount) VALUES(002, 002, 421.20);
+INSERT INTO Contributions(EID, PID, Amount) VALUES(003, 003, 2222.22);
+INSERT INTO Contributions(EID, PID, Amount) VALUES(004, 004, 1.01);
+INSERT INTO Contributions(EID, PID, Amount) VALUES(005, 005, 0.01);
+INSERT INTO Contributions(EID, PID, Amount) VALUES(006, 006, 100000.10);
+INSERT INTO Contributions(EID, PID, Amount) VALUES(999, 999, NULL);
 
 
-DROP TABLE if exists Roles;
+
+DROP TABLE IF EXISTS Roles;
 CREATE TABLE Roles (
 	RID INT PRIMARY KEY,
 	roleslist VARCHAR(255)
 );
-
 INSERT INTO Roles(RID, roleslist) VALUES (1, 'Employee');
 INSERT INTO Roles(RID, roleslist) VALUES (2, 'Volunteer');
 INSERT INTO Roles(RID, roleslist) VALUES (3, 'Adminstrator');
 INSERT INTO Roles(RID, roleslist) VALUES (4, 'General User');
 
-DROP TABLE if exists Jobs;
+DROP TABLE IF EXISTS Jobs;
 CREATE TABLE Jobs (
 	JID INT,
 	EID INT,
 	PID INT, 
 	title VARCHAR(255),
-	intime TIME null,
-	outtime TIME null,
+	intime TIME,
+	outtime TIME,
 	PRIMARY KEY (JID,EID)
 );
 
-INSERT INTO Jobs(JID, EID, PID, title, intime, outtime) VALUES (001, 001, 006, 'Teacher', '06:00:00', '14:00:00');
-INSERT INTO Jobs(JID, EID, PID, title, intime, outtime) VALUES (002, 002, 003, 'Events Organizer', '08:00:00', '16:00:00');
-INSERT INTO Jobs(JID, EID, PID, title, intime, outtime) VALUES (003, 004, 001, 'Tutor', '16:00:00', '00:00:00');
-INSERT INTO Jobs(JID, EID, PID, title, intime, outtime) VALUES (004, 003, 005, 'Youth Work', '07:00:00', '15:00:00');
-INSERT INTO Jobs(JID, EID, PID, title, intime, outtime) VALUES (005, 001, 004, 'Deliver Goods', '4:00:00', '12:00:00');
-INSERT INTO Jobs(JID, EID, PID, title, intime, outtime) VALUES (006, 006, 002, 'Cook', '06:00:00', '14:00:00');
-INSERT INTO Jobs(JID, EID, PID, title, intime, outtime) VALUES (007, 002, 005, 'Coaching', '07:00:00', '15:00:00');
-INSERT INTO Jobs(JID, EID, PID, title, intime, outtime) VALUES (008, 004, 003, 'Car Pool', '10:00:00', '18:00:00');
-INSERT INTO Jobs(JID, EID, PID, title, intime, outtime) VALUES (009, 001, 002, 'Clerk', '06:00:00', '14:00:00');
-INSERT INTO Jobs(JID, EID, PID, title, intime, outtime) VALUES (010, 004, 001, 'Music Lessons', '05:00:00', '13:00:00');
-
-
-DROP TABLE if exists Permissions;
-
-CREATE TABLE Permissions (
-    RID INT PRIMARY KEY,
-    readPerm CHAR,
-    editPerm CHAR,
-    addUserPerm CHAR,
-    updateDBPerm CHAR,
-    viewAllUserInfoPerm CHAR
+DROP TABLE IF EXISTS Permissions;
+CREATE TABLE Permissions(
+    RID INT,
+    view CHAR,
+    edit CHAR,
+    addUser CHAR,
+    updateDB CHAR,
+    viewAllUserInfo CHAR,
+    PRIMARY KEY (RID)
 );
 
-INSERT INTO Permissions(RID, readPerm, editPerm, addUserPerm, updateDBPerm, viewAllUserInfoPerm) VALUES (1, 'Y', 'Y', 'Y', 'Y', 'N');
-INSERT INTO Permissions(RID, readPerm, editPerm, addUserPerm, updateDBPerm, viewAllUserInfoPerm) VALUES (2, 'Y', 'N', 'Y', 'N', 'N');
-INSERT INTO Permissions(RID, readPerm, editPerm, addUserPerm, updateDBPerm, viewAllUserInfoPerm) VALUES (3, 'Y', 'Y', 'Y', 'Y', 'Y');
-INSERT INTO Permissions(RID, readPerm, editPerm, addUserPerm, updateDBPerm, viewAllUserInfoPerm) VALUES (4, 'Y', 'N', 'Y', 'N', 'N');
+INSERT INTO Permissions(RID, view, edit, addUser, updateDB, viewAllUserInfo) VALUES (1, 'Y', 'Y', 'Y', 'Y', 'N');
+INSERT INTO Permissions(RID, view, edit, addUser, updateDB, viewAllUserInfo) VALUES (2, 'Y', 'N', 'Y', 'N', 'N');
+INSERT INTO Permissions(RID, view, edit, addUser, updateDB, viewAllUserInfo) VALUES (3, 'Y', 'Y', 'Y', 'Y', 'Y');
+INSERT INTO Permissions(RID, view, edit, addUser, updateDB, viewAllUserInfo) VALUES (4, 'Y', 'N', 'Y', 'N', 'N');
