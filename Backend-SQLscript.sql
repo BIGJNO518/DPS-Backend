f5d9fc243aa753eaed0f9c19b0fd7c94f329c697
laborerac367 <laborerac367@strose.edu>
2018-03-31 02:07:26 -0400

    Moved inserts to the bottom of the sql script
    This is for the purpose of allowing foreign keys to work properly

--- Backend-SQLscript.sql
+++ Backend-SQLscript.sql
@@ -15,14 +15,6 @@ CREATE TABLE Users (
     last_login TIMESTAMP,
     PRIMARY KEY (PID)
 );
-INSERT INTO Users(PID, LastName, FirstName, address, phoneNumber, userEmail, RID, password) VALUES (001,'Ganoush', 'Baba', '34 Awesome Ave.', '518-775-7775', 'Yummy@food.com', 1, AES_ENCRYPT(MD5('yummy'),UNHEX(SHA2('Timmy', 512)))); 
-INSERT INTO Users(PID, LastName, FirstName, address, phoneNumber, userEmail, RID, password) VALUES (002,'Lovelace', 'Betty','37 Best Ave.', '518-665-8899', 'beez@email.com', 1,AES_ENCRYPT(MD5('worst'),UNHEX(SHA2('Spiders', 512)))); 
-INSERT INTO Users(PID, LastName, FirstName, address, phoneNumber, userEmail, RID, password) VALUES (003,'Jordan', 'Hal', '43 Lantern St.', '518-889-0099', 'GreenBaby@DC_Uni.com', 2, AES_ENCRYPT(MD5('Green'),UNHEX(SHA2('Goblin', 512))));
-INSERT INTO Users(PID, LastName, FirstName, address, phoneNumber, userEmail, RID, password) VALUES (004,'Wiggen', 'Ender','BattleSchool, Lusitania', '518-009-8877', 'xenocide@email.com', 3,AES_ENCRYPT(MD5('peace'),UNHEX(SHA2('LandWorld', 512)))); 
-INSERT INTO Users(PID, LastName, FirstName, address, phoneNumber, userEmail, RID, password) VALUES (005,'Athens, Greece', '765-957-9009', 'beannme@email.com', 2,AES_ENCRYPT(MD5('utopia'),UNHEX(SHA2('NewYorkcity', 512)))); );
-INSERT INTO Users(PID, LastName, FirstName, address, phoneNumber, userEmail, RID, password) VALUES (006,'Wiggin', 'Peter','Greensboro, NC', '800-HEGEMON', 'Hegemon@offices.earthgov', 3,AES_ENCRYPT(MD5('Yellow'),UNHEX(SHA2('Townsville', 512)))); );
-INSERT INTO Users(PID, LastName, FirstName, address, phoneNumber, userEmail, RID, password) VALUES (999, 'User', 'General', 'NULL', 'NULL', 'NULL', 4,AES_ENCRYPT(MD5('NULL'),UNHEX(SHA2('NULL1', 512)))); );
-
 
 DROP TABLE IF EXISTS Event;
 CREATE TABLE Event (
@@ -35,28 +27,12 @@ CREATE TABLE Event (
 	PRIMARY KEY(EID,Date)
 );
 
-INSERT INTO Event(EID, eventName, Date, Address, City, State) VALUES(001, 'Save the Planet', '2014-04-01', '2 This ave', 'Nowear', 'Alter');
-INSERT INTO Event(EID, eventName, Date, Address, City, State) VALUES(002, 'Save the Animals', '2012-02-05', 'Single st', 'Once', 'Ohio');
-INSERT INTO Event(EID, eventName, Date, Address, City, State) VALUES(003, 'Save the Food', '1997-08-02', 'Mono', 'Harlem', 'New York');
-INSERT INTO Event(EID, eventName, Date, Address, City, State) VALUES(004, 'Save the People', '0001-01-01', 'Jerus av', 'Truths', 'News');
-INSERT INTO Event(EID, eventName, Date, Address, City, State) VALUES(005, 'Science for the future', '1980-09-12', '2 This ave', 'Nowear', 'Alter');
-INSERT INTO Event(EID, eventName, Date, Address, City, State) VALUES(006, 'Think about the Children', '2010-06-05', '2 This ave', 'Nowear', 'Alter');
-INSERT INTO Event(EID, eventName, Date, Address, City, State) VALUES(999, 'NULL', '1899-01-01', 'NULL', 'NULL', 'NULL');
-
 DROP TABLE IF EXISTS Volunteers;
 CREATE TABLE Volunteers(
 	EID INT,
 	PID INT,
 	PRIMARY KEY(PID,EID)
 ); 
-
-INSERT INTO EventRoster(EID, PID) VALUES(001, 001);
-INSERT INTO EventRoster(EID, PID) VALUES(002, 002);
-INSERT INTO EventRoster(EID, PID) VALUES(003, 003);
-INSERT INTO EventRoster(EID, PID) VALUES(004, 004);
-INSERT INTO EventRoster(EID, PID) VALUES(005, 005);
-INSERT INTO EventRoster(EID, PID) VALUES(006, 006);
-INSERT INTO EventRoster(EID, PID) VALUES(999, 999);
 
 
 DROP TABLE IF EXISTS Contributions;
@@ -67,14 +43,6 @@ CREATE TABLE Contributions(
 	PRIMARY KEY (PID,EID)
 ); 
 
-INSERT INTO Contributions(EID, PID, Amount) VALUES(001, 001, 1000.00);
-INSERT INTO Contributions(EID, PID, Amount) VALUES(002, 002, 421.20);
-INSERT INTO Contributions(EID, PID, Amount) VALUES(003, 003, 2222.22);
-INSERT INTO Contributions(EID, PID, Amount) VALUES(004, 004, 1.01);
-INSERT INTO Contributions(EID, PID, Amount) VALUES(005, 005, 0.01);
-INSERT INTO Contributions(EID, PID, Amount) VALUES(006, 006, 100000.10);
-INSERT INTO Contributions(EID, PID, Amount) VALUES(999, 999, NULL);
-
 
 
 DROP TABLE IF EXISTS Roles;
@@ -82,10 +50,6 @@ CREATE TABLE Roles (
 	RID INT PRIMARY KEY,
 	roleslist VARCHAR(255)
 );
-INSERT INTO Roles(RID, roleslist) VALUES (1, 'Employee');
-INSERT INTO Roles(RID, roleslist) VALUES (2, 'Volunteer');
-INSERT INTO Roles(RID, roleslist) VALUES (3, 'Adminstrator');
-INSERT INTO Roles(RID, roleslist) VALUES (4, 'General User');
 
 DROP TABLE IF EXISTS Jobs;
 CREATE TABLE Jobs (
@@ -109,11 +73,6 @@ CREATE TABLE Permissions(
     PRIMARY KEY (RID)
 );
 
-INSERT INTO Permissions(RID, view, edit, addUser, updateDB, viewAllUserInfo) VALUES (1, 'Y', 'Y', 'Y', 'Y', 'N');
-INSERT INTO Permissions(RID, view, edit, addUser, updateDB, viewAllUserInfo) VALUES (2, 'Y', 'N', 'Y', 'N', 'N');
-INSERT INTO Permissions(RID, view, edit, addUser, updateDB, viewAllUserInfo) VALUES (3, 'Y', 'Y', 'Y', 'Y', 'Y');
-INSERT INTO Permissions(RID, view, edit, addUser, updateDB, viewAllUserInfo) VALUES (4, 'Y', 'N', 'Y', 'N', 'N');
-
 DROP TABLE IF EXISTS Sessions;
 CREATE TABLE Sessions (
 	SID INT NOT NULL AUTO_INCREMENT,
@@ -121,3 +80,55 @@ CREATE TABLE Sessions (
 	expires DATETIME NOT NULL, 
 	PRIMARY KEY (SID)
 	);
+	
+ALTER TABLE `sessions` ADD CONSTRAINT `sessions_fk0` FOREIGN KEY (`ID`) REFERENCES `users`(`ID`);
+
+ALTER TABLE `permissions` ADD CONSTRAINT `permissions_fk0` FOREIGN KEY (`ID`) REFERENCES `users`(`ID`);
+
+ALTER TABLE `jobs` ADD CONSTRAINT `jobs_fk0` FOREIGN KEY (`eid`) REFERENCES `events`(`ID`);
+
+ALTER TABLE `jobs` ADD CONSTRAINT `jobs_fk1` FOREIGN KEY (`uid`) REFERENCES `users`(`ID`);
+
+
+INSERT INTO Users(PID, LastName, FirstName, address, phoneNumber, userEmail, RID, password) VALUES (001,'Ganoush', 'Baba', '34 Awesome Ave.', '518-775-7775', 'Yummy@food.com', 1, AES_ENCRYPT(MD5('yummy'),UNHEX(SHA2('Timmy', 512)))); 
+INSERT INTO Users(PID, LastName, FirstName, address, phoneNumber, userEmail, RID, password) VALUES (002,'Lovelace', 'Betty','37 Best Ave.', '518-665-8899', 'beez@email.com', 1,AES_ENCRYPT(MD5('worst'),UNHEX(SHA2('Spiders', 512)))); 
+INSERT INTO Users(PID, LastName, FirstName, address, phoneNumber, userEmail, RID, password) VALUES (003,'Jordan', 'Hal', '43 Lantern St.', '518-889-0099', 'GreenBaby@DC_Uni.com', 2, AES_ENCRYPT(MD5('Green'),UNHEX(SHA2('Goblin', 512))));
+INSERT INTO Users(PID, LastName, FirstName, address, phoneNumber, userEmail, RID, password) VALUES (004,'Wiggen', 'Ender','BattleSchool, Lusitania', '518-009-8877', 'xenocide@email.com', 3,AES_ENCRYPT(MD5('peace'),UNHEX(SHA2('LandWorld', 512)))); 
+INSERT INTO Users(PID, LastName, FirstName, address, phoneNumber, userEmail, RID, password) VALUES (005,'Athens, Greece', '765-957-9009', 'beannme@email.com', 2,AES_ENCRYPT(MD5('utopia'),UNHEX(SHA2('NewYorkcity', 512)))); );
+INSERT INTO Users(PID, LastName, FirstName, address, phoneNumber, userEmail, RID, password) VALUES (006,'Wiggin', 'Peter','Greensboro, NC', '800-HEGEMON', 'Hegemon@offices.earthgov', 3,AES_ENCRYPT(MD5('Yellow'),UNHEX(SHA2('Townsville', 512)))); );
+INSERT INTO Users(PID, LastName, FirstName, address, phoneNumber, userEmail, RID, password) VALUES (999, 'User', 'General', 'NULL', 'NULL', 'NULL', 4,AES_ENCRYPT(MD5('NULL'),UNHEX(SHA2('NULL1', 512)))); );
+
+
+INSERT INTO Event(EID, eventName, Date, Address, City, State) VALUES(001, 'Save the Planet', '2014-04-01', '2 This ave', 'Nowear', 'Alter');
+INSERT INTO Event(EID, eventName, Date, Address, City, State) VALUES(002, 'Save the Animals', '2012-02-05', 'Single st', 'Once', 'Ohio');
+INSERT INTO Event(EID, eventName, Date, Address, City, State) VALUES(003, 'Save the Food', '1997-08-02', 'Mono', 'Harlem', 'New York');
+INSERT INTO Event(EID, eventName, Date, Address, City, State) VALUES(004, 'Save the People', '0001-01-01', 'Jerus av', 'Truths', 'News');
+INSERT INTO Event(EID, eventName, Date, Address, City, State) VALUES(005, 'Science for the future', '1980-09-12', '2 This ave', 'Nowear', 'Alter');
+INSERT INTO Event(EID, eventName, Date, Address, City, State) VALUES(006, 'Think about the Children', '2010-06-05', '2 This ave', 'Nowear', 'Alter');
+INSERT INTO Event(EID, eventName, Date, Address, City, State) VALUES(999, 'NULL', '1899-01-01', 'NULL', 'NULL', 'NULL');
+
+INSERT INTO EventRoster(EID, PID) VALUES(001, 001);
+INSERT INTO EventRoster(EID, PID) VALUES(002, 002);
+INSERT INTO EventRoster(EID, PID) VALUES(003, 003);
+INSERT INTO EventRoster(EID, PID) VALUES(004, 004);
+INSERT INTO EventRoster(EID, PID) VALUES(005, 005);
+INSERT INTO EventRoster(EID, PID) VALUES(006, 006);
+INSERT INTO EventRoster(EID, PID) VALUES(999, 999);
+
+INSERT INTO Contributions(EID, PID, Amount) VALUES(001, 001, 1000.00);
+INSERT INTO Contributions(EID, PID, Amount) VALUES(002, 002, 421.20);
+INSERT INTO Contributions(EID, PID, Amount) VALUES(003, 003, 2222.22);
+INSERT INTO Contributions(EID, PID, Amount) VALUES(004, 004, 1.01);
+INSERT INTO Contributions(EID, PID, Amount) VALUES(005, 005, 0.01);
+INSERT INTO Contributions(EID, PID, Amount) VALUES(006, 006, 100000.10);
+INSERT INTO Contributions(EID, PID, Amount) VALUES(999, 999, NULL);
+
+INSERT INTO Roles(RID, roleslist) VALUES (1, 'Employee');
+INSERT INTO Roles(RID, roleslist) VALUES (2, 'Volunteer');
+INSERT INTO Roles(RID, roleslist) VALUES (3, 'Adminstrator');
+INSERT INTO Roles(RID, roleslist) VALUES (4, 'General User');
+
+INSERT INTO Permissions(RID, view, edit, addUser, updateDB, viewAllUserInfo) VALUES (1, 'Y', 'Y', 'Y', 'Y', 'N');
+INSERT INTO Permissions(RID, view, edit, addUser, updateDB, viewAllUserInfo) VALUES (2, 'Y', 'N', 'Y', 'N', 'N');
+INSERT INTO Permissions(RID, view, edit, addUser, updateDB, viewAllUserInfo) VALUES (3, 'Y', 'Y', 'Y', 'Y', 'Y');
+INSERT INTO Permissions(RID, view, edit, addUser, updateDB, viewAllUserInfo) VALUES (4, 'Y', 'N', 'Y', 'N', 'N');
